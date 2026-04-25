07da1264cbed27b6672dbd1c5296044b66805bfd
jakemsr <jakemsr@yahoo.com>
2026-04-25 03:13:10 -0700

    - add cloudinary

---
+++ app/api/events/route.ts
@@ -0,0 +1,46 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { Event } from "@/database";
+import connectToDatabase from "@/lib/mongodb";
+import { File } from "buffer";
+import { get } from "http";
+import { NextRequest, NextResponse } from "next/server";
+import { resolve } from "path";
+import { buffer } from 'stream/consumers';
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectToDatabase();
+        const formData = await req.formData();
+
+        let event;
+
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch (e) {
+            return NextResponse.json({ message: 'Invalid JSON data format'}, { status: 400});
+        }
+
+        const file = formData.get('image') as File;
+        if (!file) return NextResponse.json({ message: "Image file is required" }, { status: 400 });       
+
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'events'}, (error, results) => {
+                if (error) return reject(error);
+
+                resolve(results)
+            }).end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+
+        const createdEvent = await Event.create(event);
+
+        return NextResponse.json({ message: 'Event created successfully', event: createdEvent}, { status: 201 });
+    } catch(e) {
+        console.error(e);
+        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error? e.message : 'Unknown'}, { status: 500});
+    }
+}
--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: 'res.cloudinary.com'
+      }
+    ]
+  }
 };
 
 module.exports = {
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', 'localhost'],
 }
+
 
 export default nextConfig;
