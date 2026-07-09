6fc0ee268e83e0640abf0738b8cda284c1eab610
jakemsr <jakemsr@yahoo.com>
2026-07-09 13:14:06 -0700

    refactor: Use Link instead of button with anchor

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,20 +1,18 @@
 'use client';
 
 import Image from "next/image";
+import Link from "next/link";
 
 const ExploreBtn = () => {
   return (
-      <button
-        type="button"
+      <Link
         id="explore-btn"
+        href="#events"
         className="mt-7 mx-auto"
-        onClick={() => console.log('CLICK')}
       >
-        <a href="#events">
-          Explore Events
-          <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
-        </a>
-      </button>
+        Explore Events
+        <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
+      </Link>
   )
 }
 
