51a95d857ef490fb92447363eb36be3519010f76
jakemsr <jakemsr@yahoo.com>
2026-07-12 10:01:33 -0700

    style: Whitespace fixes

--- components/SignInOutButtons.tsx
+++ components/SignInOutButtons.tsx
@@ -2,13 +2,13 @@
 
 import { useState } from "react";
 import { authClient } from "@/lib/auth-client";
-import { Button, LoadingSpinner} from "./Button";
+import { Button, LoadingSpinner } from "./Button";
 
 
 export const SignOut = () => {
 
   const [loading, setLoading] = useState(false);
-  
+
   const handleClick = async () => {
     setLoading(true);
     await authClient.signOut();
